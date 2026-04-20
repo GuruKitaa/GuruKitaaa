137b123c2381848377b1eb6f26fcb6a5b8c5c59f
Bro Fariz <idrapi4799@gmail.com>
2026-04-20 19:56:59 +0700

    Migration-Database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GuruController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Tambahkan ini
+Route::get('/gurus', [GuruController::class, 'index']);
